d2bbd81dcf356a3e3de6549faf6532f89da89d05
yasmin.elderiny <yasmin.elderiny@gmail.com>
2022-04-26 16:37:45 +0200

    Save Page hierarchy in DB

--- src/main/java/com/webcrawler/business/CrawlWebPageServiceImpl.java
+++ src/main/java/com/webcrawler/business/CrawlWebPageServiceImpl.java
@@ -2,6 +2,7 @@ package com.webcrawler.business;
 
 import com.webcrawler.model.PageInfo;
 import com.webcrawler.payload.request.CrawlPageRequest;
+import com.webcrawler.service.PageInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -18,35 +19,52 @@ import java.util.Set;
 @Slf4j
 class CrawlWebPageServiceImpl implements CrawlWebPageService {
 
+    private final PageInfoService pageInfoService;
+
     @Override
     public void crawlPage(final CrawlPageRequest crawlPageRequest) {
 
         log.info("Current Request is {}", crawlPageRequest);
-        Set<PageInfo> pageInfoSet = new HashSet<>();
-
-        retrievePageURLs(0, crawlPageRequest.getMaxLevel(), crawlPageRequest.getPageUrl(), pageInfoSet);
+        Set<String> visited = new HashSet<>();
+        visited.add(crawlPageRequest.getPageUrl());
+        retrievePageURLs(0, crawlPageRequest.getMaxLevel(), crawlPageRequest.getPageUrl(), visited,
+                null);
 
     }
 
-    private void retrievePageURLs(int currentLevel, final int maxLevel, final String pageUrl, final Set<PageInfo> pageInfoSet) {
+    private PageInfo retrievePageURLs(int currentLevel, final int maxLevel, final String pageUrl, Set<String> visited,
+                                      PageInfo parent) {
 
         if (currentLevel <= maxLevel) {
             final Document document = getChildPage(pageUrl);
             if (document != null) {
                 PageInfo pageInfo = new PageInfo();
+                pageInfo.setParentPage(parent);
                 pageInfo.setTitle(document.title());
                 pageInfo.setLevel(currentLevel);
                 for (Element e : document.select("a[href]")) {
                     String currentUrl = e.absUrl("href");
-                    if (isWithinTheSameDomain(pageUrl, currentUrl)
-                    && !currentUrl.endsWith("#")) {
-                        retrievePageURLs(currentLevel++, maxLevel, currentUrl, pageInfoSet);
+                    currentUrl = clearCurrentURL(currentUrl);
+                    if (!visited.contains(currentUrl) && isWithinTheSameDomain(pageUrl, currentUrl)) {
+                        visited.add(currentUrl);
+                        final PageInfo pageInfoSub = retrievePageURLs(currentLevel++, maxLevel, currentUrl, visited, pageInfo);
+                        pageInfo.addPageInfo(pageInfoSub);
                     }
                 }
-                pageInfoSet.add(pageInfo);
-                pageInfo.setChildPages(pageInfoSet);
+                pageInfoService.savePageInfo(pageInfo);
+                return pageInfo;
             }
         }
+        return null;
+    }
+
+    private String clearCurrentURL(final String currentUrl) {
+
+        if (currentUrl.endsWith("/#"))
+            return currentUrl.substring(0, currentUrl.length() - 2);
+        if (currentUrl.endsWith("/"))
+            return currentUrl.substring(0, currentUrl.length() - 1);
+        return currentUrl;
     }
 
     private boolean isWithinTheSameDomain(final String baseURL, final String currentUrl) {
